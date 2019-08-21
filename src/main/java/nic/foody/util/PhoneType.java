65d169f329b0cc2bd4d79f605b7de36b85c0adbc
Alex Hobson <aho113@uclive.ac.nz>
2019-08-21 15:00:29 +1200

    Removed example code

--- src/main/java/nic/foody/util/PhoneType.java
+++
@@ -1,8 +0,0 @@
-package nic.foody.util;
-
-/**
- * Simple enum to illustrate XML attribute parsingin SAX example
- */
-public enum PhoneType {
-    MOBILE, WORK, HOME, UNKNOWN
-}
--- src/main/java/nic/foody/util/ThreeValueLogic.java
+++
@@ -1,8 +0,0 @@
-package nic.foody.util;
-
-/**
- * Simple enum to illustrate three value logic for attributes
- */
-public enum ThreeValueLogic {
-    YES, NO, UNKNOWN
-}
