b1df23c55bd7f67ad1b8959e8a8f0c785d567537
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-08-21 16:45:34 +1200

    Implement the ingredient class without javadoc

--- src/main/java/foodstart/model/stock/Ingredient.java
+++ src/main/java/foodstart/model/stock/Ingredient.java
@@ -74,9 +74,61 @@ public class Ingredient
 	 * <!--  end-user-doc  -->
 	 * @generated
 	 */
-	public Ingredient(){
-		
+	public Ingredient(Unit unit, String name, String databaseId, Map<DietaryRequirement, Boolean> safeFor, int kitchenStock, int truckStock) {
+		this.unit = unit;
+		this.name = name;
+		this.databaseId = databaseId;
+		this.safeFor = safeFor;
+		this.kitchenStock = kitchenStock;
+		this.truckStock = truckStock;
 	}
 
+	public Unit getUnit() {
+		return unit;
+	}
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDatabaseId() {
+		return databaseId;
+	}
+
+	public void setDatabaseId(String databaseId) {
+		this.databaseId = databaseId;
+	}
+
+	public Map<DietaryRequirement, Boolean> getSafeFor() {
+		return safeFor;
+	}
+
+	public void setSafeFor(Map<DietaryRequirement, Boolean> safeFor) {
+		this.safeFor = safeFor;
+	}
+
+	public int getKitchenStock() {
+		return kitchenStock;
+	}
+
+	public void setKitchenStock(int kitchenStock) {
+		this.kitchenStock = kitchenStock;
+	}
+
+	public int getTruckStock() {
+		return truckStock;
+	}
+
+	public void setTruckStock(int truckStock) {
+		this.truckStock = truckStock;
+	}
 }
 
