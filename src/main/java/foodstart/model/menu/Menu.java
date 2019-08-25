ec62d96076d186cccf93dda8e09e7a0538814bf7
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-08-25 14:55:58 +1200

    Add setters and modifiers to menu class

--- src/main/java/foodstart/model/menu/Menu.java
+++ src/main/java/foodstart/model/menu/Menu.java
@@ -3,11 +3,8 @@ import java.util.Set;
 
 
 /**
- * <!-- begin-user-doc -->
- * <!--  end-user-doc  -->
- * @generated
+ * Models a menu in the system
  */
-
 public class Menu
 {
 	/**
@@ -29,23 +26,58 @@ public class Menu
 		this.menuItems = menuItems;
 		this.name = name;
 	}
+
 	/**
 	 * Returns a set of all menu items
-         * @return menuItems
+	 * @return menuItems
 	 */
-	public Set<MenuItem> getMenuItem() {
+	public Set<MenuItem> getMenuItems() {
 		return menuItems;
 	}
 	/**
 	 * Returns name of menu
-         * @return name
+	 * @return name
 	 */
-
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the menu items that make up the menu
+	 * @param menuItems the menu items that make up the menu
+	 */
+	public void setMenuItems(Set<MenuItem> menuItems) {
+		this.menuItems = menuItems;
+	}
 
+	/**
+	 * Sets the name of the menu
+	 * @param name the name of the menu
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	/**
+	 * Adds a menu item to the set of menu items in the menu
+	 * @param item the menu item to add
+	 * @return true if the item was added to the menu; false otherwise
+	 */
+	public boolean addMenuItem(MenuItem item) {
+		boolean added;
+		added = this.menuItems.add(item);
+		return added;
+	}
+
+	/**
+	 * Removes a menu item from the set of menu items in the menu
+	 * @param item the menu item to remove from the menu
+	 * @return true if the item was removed; false otherwise
+	 */
+	public boolean removeMenuItem(MenuItem item) {
+		boolean removed;
+		removed = this.menuItems.remove(item);
+		return removed;
+	}
 }
 
