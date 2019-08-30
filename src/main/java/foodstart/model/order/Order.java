b10c94647d954834e99eaea95184084f9b6d56e1
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-08-30 15:44:48 +1200

    Add getters and setters to order, and implement adding and removing items from order

--- src/main/java/foodstart/model/order/Order.java
+++ src/main/java/foodstart/model/order/Order.java
@@ -1,5 +1,6 @@
 package foodstart.model.order;
 import java.util.Map;
+import java.util.Set;
 
 import foodstart.model.PaymentMethod;
 import foodstart.model.menu.Recipe;
@@ -59,23 +60,115 @@ public class Order
 	}
 
 	/**
-	 * Calculates the total cost of an order
-	 * @return totalCost The total cost of the order
+	 * Returns the id of the order
+	 * @return the id of the order
 	 */
-	
-	public float getTotalCost() {
-		// TODO implement me
-		
-	
-		return 0.0f;
+	public String getId() {
+		return id;
 	}
 
 	/**
-	 * Adds an item to the order
+	 * Sets the id of the order
+	 * @param id the id of the order
 	 */
-	
-	public void addItem(Recipe parameter) {
-		// TODO implement me
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * Returns the map of recipes in the order to the amount of the recipe
+	 * @return the map of recipes in the order to the amount of the recipe
+	 */
+	public Map<Recipe, Integer> getItems() {
+		return items;
+	}
+
+	/**
+	 * Sets the map of recipes in the order to the amount of the recipe
+	 * @param items the map of recipes in the order to the amount of the recipe
+	 */
+	public void setItems(Map<Recipe, Integer> items) {
+		this.items = items;
+	}
+
+	/**
+	 * Returns the name of the customer
+	 * @return the name of the customer
+	 */
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	/**
+	 * Sets the name of the customer
+	 * @param customerName the name of the customer
+	 */
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	/**
+	 * Returns the time at which the order was placed
+	 * @return the time at which the order was placed
+	 */
+	public long getTimePlaced() {
+		return timePlaced;
+	}
+
+	/**
+	 * Sets the time at which the order was placed
+	 * @param timePlaced the time at which the order was placed
+	 */
+	public void setTimePlaced(long timePlaced) {
+		this.timePlaced = timePlaced;
+	}
+
+	/**
+	 * Returns the payment method for the order
+	 * @return the payment method for the order
+	 */
+	public PaymentMethod getPaymentMethod() {
+		return paymentMethod;
+	}
+
+	/**
+	 * Sets the payment method for the order
+	 * @param paymentMethod the payment method for the order
+	 */
+	public void setPaymentMethod(PaymentMethod paymentMethod) {
+		this.paymentMethod = paymentMethod;
+	}
+
+	/**
+	 * Calculates the total cost of an order
+	 * @return totalCost The total cost of the order
+	 */
+	public float getTotalCost() {
+		Set<Recipe> recipes = this.items.keySet();
+		float total = 0;
+		for (Recipe recipe : recipes) {
+			total += recipe.getPrice();
+		}
+		return total;
+	}
+
+	/**
+	 * Adds a recipe to the order. Overrides old value if the recipe already exists
+	 * @param recipe the recipe to add
+	 * @param amount the amount of the recipe to add
+	 * @return the previous amount of the recipe, or null if the recipe did not exist
+	 */
+	public Integer addItem(Recipe recipe, int amount) {
+		return this.items.put(recipe, amount);
+	}
+
+	/**
+	 * Removes a recipe from the order
+	 * @param recipe the recipe to remove
+	 * @return the amount of the recipe, or null if the recipe did not exist
+	 */
+	public Integer removeItem(Recipe recipe) {
+		return this.items.remove(recipe);
 	}
 
 }
