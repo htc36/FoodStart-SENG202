3f9d997185030d32b5d26657df4cf38e1aa63dfd
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-08-30 18:04:41 +1200

    Add setUp clause for MenuItem test

--- src/test/java/foodstart/model/menu/MenuItemTest.java
+++ src/test/java/foodstart/model/menu/MenuItemTest.java
@@ -1,14 +1,31 @@
 package foodstart.model.menu;
 
+import foodstart.model.DietaryRequirement;
+import foodstart.model.Unit;
+import foodstart.model.stock.Ingredient;
 import org.junit.Before;
 import org.junit.Test;
+import foodstart.model.menu.MenuItem;
+import foodstart.model.menu.Recipe;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static org.junit.Assert.*;
 
 public class MenuItemTest {
 
+    private MenuItem testitem;
+
     @Before
     public void setUp() throws Exception {
+        Map<DietaryRequirement, Boolean> safeFor = new HashMap<DietaryRequirement, Boolean>();
+        Ingredient ingredient = new Ingredient(Unit.GRAMS, "TestIngredient",
+                0, safeFor, 5, 10);
+        Set<Recipe> recipes = new HashSet<Recipe>();
+        testitem = new MenuItem(0, "TestItem", "MenuItem test item", recipes);
     }
 
     @Test
