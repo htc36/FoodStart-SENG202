7595133a6d555929211255514439e56d3e4589b1
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-09-02 16:25:48 +1200

    Implement the order manager

--- src/main/java/foodstart/manager/order/OrderManager.java
+++ src/main/java/foodstart/manager/order/OrderManager.java
@@ -1,6 +1,10 @@
 package foodstart.manager.order;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import foodstart.model.PaymentMethod;
+import foodstart.model.menu.Recipe;
 import foodstart.model.order.Order;
 
 
@@ -13,34 +17,45 @@ import foodstart.model.order.Order;
 public class OrderManager
 {
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * The set of all orders
 	 */
-	
-	private List<Order> allOrders;
+	private Map<Integer, Order> orders;
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
+	 * Constructs a new instance of an order manager
 	 */
 	public OrderManager(){
-		super();
+		this.orders = new HashMap<Integer, Order>();
 	}
 
 	/**
-	 * <!-- begin-user-doc -->
-	 * <!--  end-user-doc  -->
-	 * @generated
-	 * @ordered
+	 * Returns the set of all orders
+	 * @return the set of all orders
 	 */
-	
-	public List<Order> getOutstandingOrders() {
-		// TODO implement me
-		return null;
+	public Map<Integer, Order> getOrders() {
+		return orders;
 	}
 
+	/**
+	 * Gets an order from the set of orders by its UID
+	 * @param id the UID of the order to get
+	 * @return The order that the UID refers to, or null
+	 */
+	public Order getOrder(int id) {
+		return this.orders.get(id);
+	}
+
+	/**
+	 * Constructs and adds an order to the set of all orders
+	 * @param id The identifier code of the order
+	 * @param items The items that have been ordered and their quantities
+	 * @param customerName The name of the customer who made the order
+	 * @param timePlaced The time that the order was placed
+	 * @param paymentMethod The payment method that the customer chose
+	 */
+	public void addOrder(int id, Map<Recipe, Integer> items, String customerName, long timePlaced, PaymentMethod paymentMethod) {
+		Order order = new Order(id, items, customerName, timePlaced, paymentMethod);
+		this.orders.put(id, order);
+	}
 }
 
