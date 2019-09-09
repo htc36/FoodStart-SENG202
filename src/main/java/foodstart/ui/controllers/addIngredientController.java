225932866215dba64fdb8670b02135f9b50bca0c
Harry Collard <htc36@uclive.ac.nz>
2019-09-09 16:56:00 +1200

    added unit combo box to add ingredent pop up

--- src/main/java/foodstart/ui/controllers/addIngredientController.java
+++ src/main/java/foodstart/ui/controllers/addIngredientController.java
@@ -2,9 +2,13 @@ package foodstart.ui.controllers;
 
 import java.util.HashMap;
 
+import foodstart.manager.Managers;
+import foodstart.manager.stock.IngredientManager;
 import foodstart.model.DietaryRequirement;
+import foodstart.model.Unit;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 public class addIngredientController {
@@ -27,11 +31,15 @@ public class addIngredientController {
 	private CheckBox nutFree;
 	@FXML
 	private CheckBox dairyFree;
+	@FXML
+	private ComboBox UnitComboBox;
 	
 
 	@FXML
 	public void initialize(){
-
+	    UnitComboBox.getItems().removeAll(UnitComboBox.getItems());
+	    //will need to change this to use the enum rather than hardcode
+	    UnitComboBox.getItems().addAll("ml", "g", "count");
 	}
 	
 	//used to check if kitchen stock and truck stock feilds are ints
@@ -43,14 +51,21 @@ public class addIngredientController {
 			return false;
 		}
 	}
-	private void addIngredient() {
-        HashMap<DietaryRequirement, Boolean> safeFor = new HashMap<DietaryRequirement, Boolean>(); 
-        //having trouble making enum this is throwing errors
-//        safeFor.put(new DietaryRequirement("VEGAN", "vegan"), vegan.isSelected());
-//        safeFor.put(new DietaryRequirement("VEGETARIAN", "vegetarian"), vegetarian.isSelected());
-//        safeFor.put(new DietaryRequirement("GLUTEN_FREE", "gluten_free"), glutenFree.isSelected());
-//        safeFor.put(new DietaryRequirement("NUT_FREE", "nut_free"), nutFree.isSelected());
-//        safeFor.put(new DietaryRequirement("DAIRY_FREE", "dairy_free"), dairyFree.isSelected());
+	private void submit() {
+		String unitString = (String) UnitComboBox.getValue();
+		if (isInt(truckStockInput) && isInt(kitchenStockInput) && ! isInt(nameInput) && unitString != "") {
+			HashMap<DietaryRequirement, Boolean> safeFor = new HashMap<DietaryRequirement, Boolean>(); 
+//			having trouble making enum this is throwing errors
+//          safeFor.put(new DietaryRequirement("VEGAN", "vegan"), vegan.isSelected());
+//          safeFor.put(new DietaryRequirement("VEGETARIAN", "vegetarian"), vegetarian.isSelected());
+//          safeFor.put(new DietaryRequirement("GLUTEN_FREE", "gluten_free"), glutenFree.isSelected());
+//          safeFor.put(new DietaryRequirement("NUT_FREE", "nut_free"), nutFree.isSelected());
+//          safeFor.put(new DietaryRequirement("DAIRY_FREE", "dairy_free"), dairyFree.isSelected());
+			IngredientManager manager = Managers.getIngredientManager();
+			Unit unit = new Unit(unitString);
+			manager.addIngredient(unit, nameInput.getText(), idInput.getText(), safeFor, kitchenStockInput.getText(), truckStockInput.getText());
+		}
+
 
 
 
