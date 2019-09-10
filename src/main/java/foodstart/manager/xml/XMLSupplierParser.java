024387e9491e9e13b570cdd6d4cba8a7619a20b2
llo35 <llo35@uclive.ac.nz>
2019-09-10 15:46:13 +1200

    Fixed XMLSupplierParser so it parses more than one supplier.

--- src/main/java/foodstart/manager/xml/XMLSupplierParser.java
+++ src/main/java/foodstart/manager/xml/XMLSupplierParser.java
@@ -16,12 +16,12 @@ public class XMLSupplierParser extends XMLParser {
 	}
 
 	public void parse(Document doc) {
-		System.out.println("PARSING");
 
-		NodeList supplierNodes = doc.getChildNodes();
+		NodeList supplierNodes = doc.getDocumentElement().getChildNodes();
+		
 		for (int i = 0; i < supplierNodes.getLength(); i++) {
-			System.out.println("node");
 			Node node = supplierNodes.item(i);
+			
 			if (node instanceof Element) {
 				Element element = (Element) node;
 				parseOneSupplier(element);
@@ -37,7 +37,6 @@ public class XMLSupplierParser extends XMLParser {
 		PhoneType phoneType = PhoneType.matchType(element.getAttribute("type"));
 		String email = element.getElementsByTagName("email").item(0).getTextContent();
 		String url = element.getElementsByTagName("url").item(0).getTextContent();
-		System.out.println(name + " " + address + " " + url);
 		Managers.getSupplierManager().addSupplier(sid, name, phone, phoneType, email, url, address);
 	}
 	
