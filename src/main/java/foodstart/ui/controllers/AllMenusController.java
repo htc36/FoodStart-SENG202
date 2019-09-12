51d730cafed82d7602131576798e2936d2049919
fryfrankster <42592731+fryfrankster@users.noreply.github.com>
2019-09-12 16:02:10 +1200

    Added AllMenusController Class and the allMenus.fxml Frankie and Lydia

---
+++ src/main/java/foodstart/ui/controllers/AllMenusController.java
@@ -0,0 +1,47 @@
+package foodstart.ui.controllers;
+
+import foodstart.manager.Managers;
+import foodstart.model.menu.Menu;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.MenuButton;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.paint.Color;
+
+import java.util.Set;
+
+public class AllMenusController {
+    @FXML
+    private FlowPane flowPane;
+
+    @FXML
+    private MenuButton menuButton;
+
+    /**
+     * Box background for menu items in the grid
+     */
+    private Background boxBackground;
+
+
+    public void initialize() {
+        boxBackground = new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY));
+        //populateAllMenus(flowPane);
+
+    }
+
+    /**
+     * Populate the FlowPane with all menu items
+     *
+     * @param flowPane
+     *            The flowpane to populate
+     */
+    public void populateAllMenus(FlowPane flowPane) {
+        flowPane.getChildren().clear();
+//        for (Set<Menu> menu : Managers.getMenuManager().getMenus()) {
+//            flowPane.getChildren().add(createMenuItemBox(menu));
+//        }
+    }
+}
