63c24acf8fa06e1a903691dfd1c723ef0ec15f61
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-09-12 17:37:55 +1200

    Add ignore annotations for unimplemented tests

--- src/test/java/foodstart/managers/menu/MenuItemManagerTest.java
+++ src/test/java/foodstart/managers/menu/MenuItemManagerTest.java
@@ -4,10 +4,7 @@ import static org.junit.Assert.*;
 
 import java.util.HashMap;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
+import org.junit.*;
 
 import foodstart.manager.menu.MenuItemManager;
 import foodstart.model.menu.PermanentRecipe;
@@ -16,10 +13,10 @@ import foodstart.model.stock.Ingredient;
 public class MenuItemManagerTest {
     //UNFINISHED
     private MenuItemManager testManager;
-    private PermanentRecipe recipe1, recipe, recipeSilly, recipeEmpty, recipeNull;
+    private static PermanentRecipe recipe1, recipe, recipeSilly, recipeEmpty, recipeNull;
     
     @BeforeClass
-    public void setUpBeforeClass() throws Exception {
+    public static void setUpBeforeClass() throws Exception {
         HashMap<Ingredient, Integer> dummyVariations = new HashMap<Ingredient, Integer>();
         recipe1 = new PermanentRecipe(1, "test1", "do some stuff", (float) 10.5, dummyVariations);
         recipe1 = new PermanentRecipe(2, "test2", "do some more stuff", (float) 0.65, dummyVariations);
@@ -30,40 +27,48 @@ public class MenuItemManagerTest {
                 (float) 46846849852416.5524736, dummyVariations);
         }
 
+    @Ignore
     @Before
     public void setUp() throws Exception {
         testManager = new MenuItemManager();
     }
 
+    @Ignore
     @After
     public void tearDown() throws Exception {
     }
 
+    @Ignore
     @Test
     public void testAddMenuItem() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenuItem() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenuItems() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenuItemsCollectionOfInteger() {
         fail("Not yet implemented");
     }
 
     @Test
+    @Ignore
     public void testGetMenuItemSet() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetApproxPrice() {
         fail("Not yet implemented");
--- src/test/java/foodstart/managers/menu/MenuManagerTest.java
+++ src/test/java/foodstart/managers/menu/MenuManagerTest.java
@@ -4,38 +4,46 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class MenuManagerTest {
 
+    @Ignore
     @Before
     public void setUp() throws Exception {
     }
 
+    @Ignore
     @After
     public void tearDown() throws Exception {
     }
 
+    @Ignore
     @Test
     public void testAddMenu() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenu() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenus() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenusCollectionOfInteger() {
         fail("Not yet implemented");
     }
 
+    @Ignore
     @Test
     public void testGetMenuSet() {
         fail("Not yet implemented");
