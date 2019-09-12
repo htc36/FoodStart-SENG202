7949276edcd63ef359dff16726148af08562c951
Alan Wang <awa111@uclive.ac.nz>
2019-09-12 15:57:33 +1200

    # WARNING: head commit changed in the meantime
    Removing old "Latex" directory

---
+++ src/test/java/foodstart/managers/menu/MenuManagerTest.java
@@ -0,0 +1,44 @@
+package foodstart.managers.menu;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MenuManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void testAddMenu() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMenu() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMenus() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMenusCollectionOfInteger() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetMenuSet() {
+        fail("Not yet implemented");
+    }
+
+}
