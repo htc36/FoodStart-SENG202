e180c0737d89469dee473612cdf581bb609350eb
Hamish O'Keeffe <hoo42@uclive.ac.nz>
2019-09-17 13:40:43 +1200

    Implement most of the sales log parser, as it still needs OTF recipe parsing

--- src/main/java/foodstart/manager/xml/XMLSalesLogParser.java
+++ src/main/java/foodstart/manager/xml/XMLSalesLogParser.java
@@ -1,14 +1,17 @@
 package foodstart.manager.xml;
 
-import foodstart.model.DataType;
 import foodstart.manager.Managers;
+import foodstart.manager.exceptions.IDLeadsNowhereException;
+import foodstart.model.DataType;
+import foodstart.model.PaymentMethod;
+import foodstart.model.menu.Recipe;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import java.time.Month;
-import java.time.Year;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class XMLSalesLogParser extends XMLParser {
@@ -23,7 +26,6 @@ public class XMLSalesLogParser extends XMLParser {
     public void parse(Document doc) {
         //TODO: Finish implementing parser
         NodeList salesNodes = doc.getChildNodes();
-        System.out.println(salesNodes);
         for (int j = 0; j < salesNodes.getLength(); j++) {
             if (salesNodes.item(j) instanceof Element && salesNodes.item(j).getNodeName().equalsIgnoreCase("sales")) {
                 if (salesNodes.item(0).getNodeName().equalsIgnoreCase("sales")) {
@@ -40,19 +42,38 @@ public class XMLSalesLogParser extends XMLParser {
         }
     }
 
+    private Map<Recipe, Integer> getSaleItems(Element element) {
+    	//TODO: Add parsing for OTF recipes
+	    Map<Recipe, Integer> recipes = new HashMap<Recipe, Integer>();
+	    NodeList recipeNodes = element.getElementsByTagName("recipes").item(0).getChildNodes();
+	    for (int i = 0; i < recipeNodes.getLength(); i++) {
+		    Node node = recipeNodes.item(i);
+		    if (node.getNodeName().equalsIgnoreCase("recipe")) {
+		    	Element recipeEl = (Element) node;
+			    int recipeId = Integer.parseInt(recipeEl.getElementsByTagName("recipe_id").item(0).getTextContent());
+			    int quantity = Integer.parseInt(recipeEl.getElementsByTagName("quantity").item(0).getTextContent());
+			    Recipe recipe = Managers.getRecipeManager().getRecipe(recipeId);
+			    if (recipe == null) {
+				    throw new IDLeadsNowhereException(DataType.RECIPE, recipeId);
+			    }
+			    recipes.put(recipe, quantity);
+		    }
+	    }
+	    return recipes;
+    }
     /**
      * Parses one sale from the given element
      *
      * @param element XML Element to parse
      */
     private void parseOneSale(Element element) {
-        System.out.println("Parsing");
         int id = Integer.parseInt(element.getElementsByTagName("id").item(0).getTextContent());
         String name = element.getElementsByTagName("name").item(0).getTextContent();
         long date = Long.parseLong(element.getElementsByTagName("date").item(0).getTextContent());
         float cost = Float.parseFloat(element.getElementsByTagName("cost").item(0).getTextContent());
-
-
+	    PaymentMethod payment = PaymentMethod.matchNiceName(element.getElementsByTagName("payment").item(0).getTextContent());
+	    Map<Recipe, Integer> recipes = getSaleItems(element);
+		Managers.getOrderManager().addOrder(id, recipes, name, date, payment, cost);
 
     }
 }
