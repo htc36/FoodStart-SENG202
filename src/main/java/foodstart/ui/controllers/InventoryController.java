81471c41a4a82c2c11ca513ae8822f3e812c9d8f
Harry Collard <htc36@uclive.ac.nz>
2019-09-19 15:41:39 +1200

    Made progress on edit ingredient pop up

--- src/main/java/foodstart/ui/controllers/InventoryController.java
+++ src/main/java/foodstart/ui/controllers/InventoryController.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import foodstart.manager.Managers;
 import foodstart.manager.stock.IngredientManager;
+import foodstart.model.order.Order;
 import foodstart.model.stock.Ingredient;
 import foodstart.ui.Refreshable;
 import javafx.beans.property.SimpleStringProperty;
@@ -13,6 +14,8 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
@@ -32,6 +35,7 @@ public class InventoryController implements Refreshable {
 	@FXML
 	private TableColumn<Ingredient, String> dietary;
 	private FXMLLoader loader;
+	private Stage popupStage;
 
 	/**
 	 * Observable list of ingredients
@@ -81,6 +85,48 @@ public class InventoryController implements Refreshable {
 		stage.setScene(scene);
 		stage.show();
 	}
+	public void editIngredient2() {
+		loader = new FXMLLoader(getClass().getResource("editIngredient.fxml"));
+		try {
+			loader.load();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Scene scene = new Scene(loader.getRoot());
+		stage = new Stage();
+		stage.setTitle("Edit Item");
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	public void editIngredient() {
+		Ingredient ingredient = inventoryView.getSelectionModel().getSelectedItem();
+		if (ingredient == null) {
+			Alert alert = new Alert(Alert.AlertType.WARNING, "Could not edit ingredient as nothing is selected", ButtonType.OK);
+			alert.setHeaderText("No order selected");
+			alert.showAndWait();
+		} else {
+//			if (popupStage.getOwner() == null) {
+//				popupStage.initOwner(this.inventoryView.getScene().getWindow());
+//			}
+			loader = new FXMLLoader(getClass().getResource("editIngredient.fxml"));
+			try {
+				loader.load();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			((EditIngredientController) loader.getController()).setIngredient(ingredient);
+			Scene scene = new Scene(loader.getRoot());
+			stage = new Stage();
+			stage.setTitle("Edit Item");
+			stage.setScene(scene);
+			stage.show();
+			refreshTable();
+		}
+	}
+
 	public void closeAddIngredient() {
 		stage.close();
 	}
