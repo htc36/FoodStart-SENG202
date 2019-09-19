1b48750e3b4fd129fb10d8f451cf0452e6506c17
Alex Hobson <aho113@uclive.ac.nz>
2019-09-19 16:16:57 +1200

    Create tests for menu xml parsing

---
+++ src/test/java/foodstart/manager/xml/XMLMenuParserTest.java
@@ -0,0 +1,73 @@
+package foodstart.manager.xml;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import foodstart.manager.Managers;
+import foodstart.manager.Persistence;
+import foodstart.model.DataType;
+import foodstart.model.menu.MenuItem;
+import foodstart.model.menu.PermanentRecipe;
+import foodstart.model.stock.Ingredient;
+
+public class XMLMenuParserTest {
+
+	Persistence persistence;
+	
+	@BeforeClass
+	public static void setupRecipes() {
+		Managers.getRecipeManager().addRecipe(1000, "Burger", "Test instructions", 5.00F, new HashMap<Ingredient, Integer>());
+		Managers.getRecipeManager().addRecipe(1001, "Grass burger", "Test instructions", 8.00F, new HashMap<Ingredient, Integer>());
+	}
+	
+	@Before
+	public void setUp() throws Exception {
+		persistence = new XMLPersistence();
+		persistence.importFile(new File("resources/data/TestMenu1.xml"), DataType.MENU);
+	}
+
+	@Test
+	public void testMenuExists() {
+		assertTrue("Menu with ID 0 should exist", Managers.getMenuManager().getMenu(0) != null);
+	}
+	
+	@Test
+	public void testMenuTitle() {
+		assertEquals("Menu with ID 0 should have a name", "Test title", Managers.getMenuManager().getMenu(0).getTitle());
+	}
+	
+	@Test
+	public void testMenuDescription() {
+		assertEquals("Menu with ID 0 should have a description", "Test description", Managers.getMenuManager().getMenu(0).getDescription());
+	}
+	
+	@Test
+	public void testMenuItems() {
+		assertEquals("Menu with ID 0 should have an item", 1, Managers.getMenuManager().getMenu(0).getMenuItems().size());
+		MenuItem item = Managers.getMenuManager().getMenu(0).getMenuItems().iterator().next();
+		assertEquals("Should have an item with ID 0", 0, item.getId());
+		assertEquals("Should have an item with name 'Boring Burger'", "Boring Burger", item.getName());
+		assertEquals("Should have an item with description 'Test burger'", "Test burger", item.getDescription());
+		
+		List<PermanentRecipe> variants = item.getVariants();
+		assertEquals("Item should have 2 recipes", 2, variants.size());
+		assertTrue("Item should have a recipe with ID 1000", variants.get(0).getId() == 1000 || variants.get(1).getId() == 1000);
+		assertTrue("Item should have a recipe with ID 1001", variants.get(0).getId() == 1001 || variants.get(1).getId() == 1001);
+		assertTrue("Items should be in the same order as in XML", variants.get(0).getId() == 1000 && variants.get(1).getId() == 1001);
+	}
+	
+	@Test
+	public void testImportMultipleMenus() {
+		persistence.importFile(new File("resources/data/TestMenu2.xml"), DataType.MENU);
+		assertTrue("Menu with ID 1 should exist", Managers.getMenuManager().getMenu(1) != null);
+		assertTrue("Menu with ID 2 should exist", Managers.getMenuManager().getMenu(2) != null);
+	}
+}
