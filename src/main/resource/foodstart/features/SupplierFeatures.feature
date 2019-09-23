763ce3e4803f4d7e2c87e6825d50b4b52f8850b6
Lydia Looi <llo35@uclive.ac.nz>
2019-09-23 18:58:15 +1200

    Revised the add supplier feature and implemented some steps.

--- src/main/resource/foodstart/features/SupplierFeatures.feature
+++ src/main/resource/foodstart/features/SupplierFeatures.feature
@@ -1,57 +1,60 @@
-Feature: Supplier feature.  
+Feature: Supplier feature
     Some example scenarios involving suppliers
 
-    @skip_scenario
-    Scenario: Adding a supplier to the suppliers list (FR10)
-        Given Supplier "Count Up" does not exist in the supplier list
-        And Its code is 123
-        And Its phone number is "0123456789"
+
+    Scenario: Adding a supplier to the suppliers list (UC13)
+        Given There are 2 suppliers in the suppliers list
+        And Supplier with code 1807 does not exist in the supplier list
+        And Its code is 1807
+        And Its name is "Pak'n'Slave"
+        And Its phone number is "025031807"
         And Its phone type is "WORK"
-        And Its email is "onetwothree@countup.com"
-        And Its website is "www.countup.com"
-        And Its address is "1234 Five Road"
-        When Supplier "Count Up" is manually added to the supplier list
-        Then It will be stored under "Count Up" in the supplier list
+        And Its email is "trade@paknslave.com"
+        And Its website is "www.paknslave.com"
+        And Its address is "25 Traders Road"
+        When Supplier with code 1807 is manually added to the supplier list
+        Then It will be stored with code 1807 in the supplier list
+        And Its name will be "Pak'n'Slave"
+        And Its phone number will be "025031807"
+        And Its phone type will be "WORK"
+        And Its email will be "trade@paknslave.com"
+        And Its website will be "www.paknslave.com"
+        And Its address will be "25 Traders Road"
 
     @skip_scenario # Exception has not been created yet
-    Scenario: Adding an existing supplier to the suppliers list (FR10)
-        Given Supplier "Old World" exists in the supplier list
-        And Its code is 12
+    Scenario: Adding an existing supplier to the suppliers list (UC13)
+        Given Supplier with code 12 exists in the supplier list
+        And Its name is "Old World"
         And Its phone number is "01430414"
         And Its phone type is "MOBILE"
         And Its email is "awhole@oldworld.com"
         And Its website is "www.oldworld.com"
         And Its address is "123 Great Road"
-        When Supplier "Old World" is manually added to the supplier list
+        When Supplier with code 12 is manually added to the supplier list
         Then An ExistingSupplierException is thrown
 
     @skip_scenario
-    Scenario: Removing a supplier from the suppliers list (FR10)
-        Given Supplier "Pak'n'Slave" exists in the supplier list
-        And Its code is 1807
-        And Its phone number is "0800123456"
-        And Its phone type is "WORK"
-        And Its email is "store@paknslave.com"
-        And Its website is "www.oldworld.com"
-        And Its address is "187 Traders Road"
-        When Supplier "Pak'n'Slave' is manually removed from the supplier list
-        Then It is removed from the database and cannot be viewed on the list
+    Scenario: Removing a supplier from the suppliers list (UC13)
+        Given There are 2 suppliers in the suppliers list
+        And Supplier with code 1807 exists in the supplier list
+        When Supplier with code 1807 is manually removed from the supplier list
+        Then Supplier with code 1807 will not exist in the supplier list
 
     @skip_scenario
-    Scenario: View suppliers (FR16)
+    Scenario: View suppliers (UC8)
         Given An employee wants to view the suppliers list
         And There are 2 suppliers in the suppliers list
         When The suppliers list is displayed
         Then The code, name, phone number, phone type, email, website and address for all 2 suppliers are displayed
 
     @skip_scenario
-    Scenario: Missing fields (FR9)
-        Given Supplier "Count Up" does not exist in the supplier list
-        And Its code is 123
-        And Its phone number is null
+    Scenario: Missing fields (UC13)
+        Given Supplier with code 123 does not exist in the supplier list
+        And Its name is "Count Up"
+        And Its phone number is ""
         And Its phone type is "WORK"
         And Its email is "onetwothree@countup.com"
         And Its website is "www.countup.com"
         And Its address is "1234 Five Road"
-        When The "Count Up" is loaded
+        When The supplier with code 123 is loaded
         Then An ImportFailureException is thrown
--- src/test/java/foodstart/acceptance_tests/step_definitions/SupplierSteps.java
+++ src/test/java/foodstart/acceptance_tests/step_definitions/SupplierSteps.java
@@ -1,10 +1,151 @@
 package foodstart.acceptance_tests.step_definitions;
 
+import foodstart.manager.stock.SupplierManager;
+import foodstart.model.PhoneType;
+import foodstart.model.stock.Supplier;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class SupplierSteps {
+import java.util.HashMap;
 
+import static junit.framework.TestCase.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotNull;
+
+public class SupplierSteps {
+    private SupplierManager supplierManager = new SupplierManager();
+    private HashMap<Integer, Supplier> existingSupplierSet;
+
+    private Integer code;
+    private String name;
+    private String phoneNumber;
+    private PhoneType phoneType;
+    private String email;
+    private String website;
+    private String address;
+
+    /**
+     * Creates a set of suppliers that can be added to the supplier list before testing if needed
+     * @return
+     */
+    private void createSupplierSet() {
+        HashMap<Integer, Supplier> supplierSet = new HashMap<Integer, Supplier>();
+        Supplier CountUp = new Supplier(123, "Count Up", "0123456789", PhoneType.WORK,
+                "onetwothree@countup.com", "www.countup.com", "1234 Five Road");
+        supplierSet.put(0, CountUp);
+
+        Supplier UnderValue = new Supplier(1, "Under Value", "01111111", PhoneType.WORK,
+                "super@undervalue.com", "www.undervalue.com", "11 Costly Road");
+        supplierSet.put(1, UnderValue);
+
+        existingSupplierSet = supplierSet;
+    }
+
+    @Given("There are {int} suppliers in the suppliers list")
+    public void thereAreSuppliersInTheSuppliersList(Integer numberOfSuppliers) {
+        createSupplierSet();
+        for (int i = 0; i < numberOfSuppliers; i++) {
+            supplierManager.addSupplier(existingSupplierSet.get(i));
+        }
+    }
+
+    @Given("Supplier with code {int} does not exist in the supplier list")
+    public void supplierWithCodeDoesNotExistInTheSupplierList(Integer supplierCode) {
+        assertNull(supplierManager.getSupplier(supplierCode));
+    }
+
+    @Given("Its code is {int}")
+    public void itsCodeIs(Integer supplierCode) {
+        code = supplierCode;
+    }
+
+    @Given("Its name is {string}")
+    public void itsNameIs(String supplierName) {
+        name = supplierName;
+    }
+
+    @Given("Its phone number is {string}")
+    public void itsPhoneNumberIs(String supplierPhoneNumber) {
+        phoneNumber = supplierPhoneNumber;
+    }
+
+    @Given("Its phone type is {string}")
+    public void itsPhoneTypeIs(String supplierPhoneType) {
+        phoneType = PhoneType.matchType(supplierPhoneType);
+    }
+
+    @Given("Its email is {string}")
+    public void itsEmailIs(String supplierEmail) {
+        email = supplierEmail;
+    }
+
+    @Given("Its website is {string}")
+    public void itsWebsiteIs(String supplierWebsite) {
+        website = supplierWebsite;
+    }
+
+    @Given("Its address is {string}")
+    public void itsAddressIs(String supplierAddress) {
+        address = supplierAddress;
+    }
+
+    @When("Supplier with code {int} is manually added to the supplier list")
+    public void supplierIsManuallyAddedToTheSupplierList(Integer supplierCode) {
+        supplierManager.addSupplier(supplierCode, name, phoneNumber, phoneType, email, website, address);
+    }
+
+    @Then("It will be stored with code {int} in the supplier list")
+    public void itWillBeStoredUnderInTheSupplierList(Integer supplierCode) {
+        assertNotNull(supplierManager.getSupplier(supplierCode));
+    }
+
+    @Then("Its name will be {string}")
+    public void itsNameWillBe(String supplierName) {
+        assertEquals(supplierName, supplierManager.getSupplier(code).getSupplierName());
+    }
+
+    @Then("Its phone number will be {string}")
+    public void itsPhoneNumberWillBe(String supplierPhoneNumber) {
+        assertEquals(supplierPhoneNumber, supplierManager.getSupplier(code).getPhoneNumber());
+    }
+
+    @Then("Its phone type will be {string}")
+    public void itsPhoneTypeWillBe(String supplierPhoneType) {
+        assertEquals(PhoneType.matchType(supplierPhoneType), supplierManager.getSupplier(code).getPhoneType());
+    }
+
+    @Then("Its email will be {string}")
+    public void itsEmailWillBe(String supplierEmail) {
+        assertEquals(supplierEmail, supplierManager.getSupplier(code).getEmail());
+    }
+
+    @Then("Its website will be {string}")
+    public void itsWebsiteWillBe(String supplierWebsite) {
+        assertEquals(supplierWebsite, supplierManager.getSupplier(code).getUrl());
+    }
+
+    @Then("Its address will be {string}")
+    public void itsAddressWillBe(String supplierAddress) {
+        assertEquals(supplierAddress, supplierManager.getSupplier(code).getAddress());
+    }
+
+    @Given("Supplier with code {int} exists in the supplier list")
+    public void supplierWithCodeExistsInTheSupplierList(Integer supplierCode) {
+        assertNotNull(supplierManager.getSupplier(supplierCode));
+    }
+
+
+    @When("Supplier with code {int} is manually removed from the supplier list")
+    public void supplierWithCodeIsManuallyRemovedFromTheSupplierList(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("Supplier with code {int} will not exist in the supplier list")
+    public void supplierWithCodeWillNotExistInTheSupplierList(Integer int1) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
 
 }
